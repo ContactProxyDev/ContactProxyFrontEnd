183a35b6f5a151c4b102cd0b3070dac172c74b91
punch-bob <rybalkokostyan@gmail.com>
2022-11-06 13:58:55 +0700

    [CONTACT-PROXY-T-29]: added main page without design, only for tests

---
+++ src/modules/index/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button style="margin-bottom: 20px;" href="./../../../registration.html">Reg</button><br>
+    <button href="./../../../auth.html">Auth</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/modules/index/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
+
+import '../../style.css'
+
+createApp(App).use(createVuestic()).mount('#app')
