8f5f9b690eba08d9eb136d29e2d1c7240668dcba
punch-bob <rybalkokostyan@gmail.com>
2022-11-06 14:22:02 +0700

    [CONTACT-PROXY-T-29]: fixed error opening pages

--- src/modules/index/App.vue
+++ src/modules/index/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <button style="margin-bottom: 20px;" href="./../../../registration.html">Reg</button><br>
-    <button href="./../../../auth.html">Auth</button>
+    <va-button style="margin-bottom: 20px;" href="./../../../registration.html">Reg</va-button><br>
+    <va-button href="./../../../auth.html">Auth</va-button>
   </div>
 </template>
 
